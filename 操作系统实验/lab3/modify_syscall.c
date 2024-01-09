5bcdd4e98d32f9f7748be5292756e415d14f186c
Ruqi Fu <2425361434@qq.com>
2024-01-09 21:54:45 +0800

    op_2

--- 操作系统实验/lab3/modify_syscall.c
+++ 操作系统实验/lab3/modify_syscall.c
@@ -14,34 +14,34 @@ unsigned long orig_cr0;
 
 unsigned int clear_cr0(void)        
 {
-    unsigned int cr0 = 0;
-    unsigned int ret;
-    //move the value in reg cr0 to reg rax
-    //movl moves a 32-bits operand
-    //movq moves a 64-bits operand
-    //rax is a 64-bits register
-    //an assembly language code
-    //asm volatile ("movl %%cr0, %%eax" : "=a"(cr0));//32-bits        
-    asm volatile ("movq %%cr0, %%rax" : "=a"(cr0));        //64-bits
-    ret = cr0;
-    //var cr0 is rax        
-    cr0 &= 0xfffeffff; //set 0 to the 16th bit
-    //asm volatile ("movl %%eax, %%cr0" :: "a"(cr0));//32-bits
-    //note that cr0 above is a variable while cr0 below is a reg.        
-    asm volatile ("movq %%rax, %%cr0" :: "a"(cr0));        
-    return ret;
+        unsigned int cr0 = 0;
+        unsigned int ret;
+        //move the value in reg cr0 to reg rax
+        //movl moves a 32-bits operand
+        //movq moves a 64-bits operand
+        //rax is a 64-bits register
+        //an assembly language code
+        //asm volatile ("movl %%cr0, %%eax" : "=a"(cr0));//32-bits        
+        asm volatile ("movq %%cr0, %%rax" : "=a"(cr0));        //64-bits
+        ret = cr0;
+        //var cr0 is rax        
+        cr0 &= 0xfffeffff; //set 0 to the 17th bit
+        //asm volatile ("movl %%eax, %%cr0" :: "a"(cr0));//32-bits
+        //note that cr0 above is a variable while cr0 below is a reg.        
+        asm volatile ("movq %%rax, %%cr0" :: "a"(cr0));        
+        return ret;
 }
 
 //recover the value of WP 
 void setback_cr0(unsigned int val)
 {        
-    //asm volatile ("movl %%eax, %%cr0" :: "a"(val));//32-bits
-    asm volatile ("movq %%rax, %%cr0" :: "a"(val));//64-bits
+        //asm volatile ("movl %%eax, %%cr0" :: "a"(val));//32-bits
+        asm volatile ("movq %%rax, %%cr0" :: "a"(val));//64-bits
 }
 
 asmlinkage int hello(int a,int b) //new function
 {
-    printk("No 96 syscall has changed to hello");
+    printk("No 78 syscall has changed to hello");
     return a + b;
 }
 
@@ -52,7 +52,7 @@ void modify_syscall(void)
 	if (!p_sys_call_table) {
         pr_err("Failed to find sys_call_table address\n");
         return;
-    }
+    	}
         
     sys_call_addr = (unsigned long *)(p_sys_call_table + sys_No * sizeof(void *));
     old_sys_call_func = *(sys_call_addr);
@@ -68,7 +68,7 @@ void restore_syscall(void)
     sys_call_addr = (unsigned long *)(p_sys_call_table + sys_No * sizeof(void *));
 	orig_cr0 = clear_cr0();
     *(sys_call_addr) = old_sys_call_func; // point to original function
-    setback_cr0(orig_cr0);
+setback_cr0(orig_cr0);
 }
 
 static int mymodule_init(void)
